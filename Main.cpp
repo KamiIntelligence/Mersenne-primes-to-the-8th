88239b1878d805f5de6700c79691aff5c76597c5
Q' - 2 <q'push2@forward.cat>
2016-10-07 19:50:08 -0400

    Attempting to use a BigNum library...
    Linker errors behold.

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
-/* #include <gmpxx.h>
- * I use intergers larger than unsigned long long.
- * - Never mind I forgot just how abhorrant the GMP interface for C++ is.
- * Ergo doing the bare minimum calculating the 8th Mersenne prime. */
+#include "BigIntegerLibrary.hh"
 #include <vector>
 
 /* Non-scalable for BigNum values, use GMP mpz_ui_pow_ui if you need bigger. */
-unsigned long long int integerOrder(int base, int order)
-{
-  unsigned long long int r = 1;
+BigUnsigned integerOrder(int base, int order)
+{ int r = 1;
   while (order--)
     r *= base;
-  return r;
+  return static_cast<BigUnsigned>(r);
 }
 
 /* The algorithm in isPrime is broken.
@@ -21,8 +17,8 @@ unsigned long long int integerOrder(int base, int order)
  * Assume primes are all odd.
  * Oh, and the efficiency of the algorithm is completely dependent on how big
  * possiblePrime gets. It beats the sqrt(possiblePrime) in benchmarks. */
-bool isPrime(unsigned long long int possiblePrime)
-{ for (unsigned long long int i = 3; i * i <= possiblePrime; i += 2)
+bool isPrime(BigUnsigned possiblePrime)
+{ for (BigUnsigned i = 3; i * i <= possiblePrime; i += 2)
   { if (possiblePrime % i == 0)
     return false;
   }
@@ -31,36 +27,15 @@ bool isPrime(unsigned long long int possiblePrime)
 
 int main(void)
 {
-  /* Vectors to store our data, GMP does not implement nicely into them ergo
-   * if you want to use GMP, const sized arrays are your friend. */
-  std::vector<unsigned long long int> possibleMersennePrimes;
-  std::vector<int> exponents;
-  for (int i = 2; i <= 63; i++) /* 2^61 - 1 calculates the 8th Mersenne Prime,
-                                 * higher, and beyond use GMP.
-                                 * The max capable with primative data types is:
-                                 * 18446744073709551615
-                                 * which is < 618970019642690137449562111.
-                                 * Though one of course could be like
-                                 * C programmers and use char[] for everything.
-                                 */
+  for (int i = 2; i <= 100; i++)
   {
-    if (i % 2 == 1 || i == 2) /* Mersenne Primes have not appeared for even
-                               * numbers (except 2), so this speeds up some computation.*/
-    { unsigned long long int temp = (integerOrder(2, i) - 1);
-      possibleMersennePrimes.push_back(temp);
-      exponents.push_back(i); /* So we can tell the user what Mersenne prime. */
+    if ((i & 1) == 1 || i == 2)
+    { BigUnsigned temp = (integerOrder(2, i) - 1);
+	  if (isPrime(temp))
+		  std::cout << "2^" << i << " - 1 is prime: " << temp << std::endl;
     }
   }
-  /* Output, aka listing what is and is not primes to the terminal/console. */
-  std::cout << "A list of our possible Mersenne primes: " << std::endl;
-  for (unsigned int i = 0; i < possibleMersennePrimes.size(); i++)
-  {
-    if (isPrime(possibleMersennePrimes[i]))
-      std::cout << "2^" << exponents[i] << " - 1 is prime: "
-                << possibleMersennePrimes[i] << std::endl;
-    else
-      std::cout << "2^" << exponents[i] << " - 1 is NOT prime: "
-                << possibleMersennePrimes[i] << std::endl;
-  }
+  std::cout << "Press enter to quit...";
+  std::getwchar();
   return 0; /* C++11 automatically does this, however... */
 }
