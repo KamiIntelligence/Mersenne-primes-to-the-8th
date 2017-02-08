7f8acf4d8c7d9cce280fd032e75fc63ef77d8795
S <S-NA@users.noreply.github.com>
2017-02-08 13:22:02 -0500

    I am too tired to write a proper commit message.

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -3,20 +3,21 @@ module Main where
 import Data.List (elemIndices)
 import Math.NumberTheory.Primes.Testing
 
-oddNumbers = [1,3..1000000]
+oddNumbers = [1,3..256]
 possibleMersennePrimes = [2^n - 1 | n <- oddNumbers]
 
 locationOfMersennePrimes = map isPrime possibleMersennePrimes
 
 elementLocationsOfPrimes = elemIndices True locationOfMersennePrimes
 thePrimeMakingExponents = [oddNumbers !! n | n <- elementLocationsOfPrimes]
-theMersennePrimes = [2^n - 1 | n <- elementLocationsOfPrimes]
+theMersennePrimes = [2^n - 1 | n <- thePrimeMakingExponents]
+
+formattedList = [n | n <- zip thePrimeMakingExponents theMersennePrimes]
 
 main = do
     putStrLn "A list of odd numbers, since there is only one Mersenne prime with and even exponent."
     putStrLn "Raise each element in our odd numbers list to get a list of possible Mersenne primes."
-    putStrLn "Found primes: "
-    print (theMersennePrimes)
-    putStrLn "Their exponents: "
+    display formattedList
     print (thePrimeMakingExponents)
+    print (theMersennePrimes)
     putStrLn "\n"
